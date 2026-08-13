48fd64a956b378a891edb7f8b6b6e39a16e8ec49
Yasseen Ahmed <Yasseen.ahmed2005@gmail.com>
2026-08-13 07:29:25 +0300

    Project

---
+++ Src/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Application Entry Point
+ */
+
+int main(void)
+{
+    /* Application Initialization */
+
+    while(1)
+    {
+        /* Main Application Loop */
+    }
+
+    return 0;
+}
